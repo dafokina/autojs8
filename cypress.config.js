b3e4f9066da09dc059285ae88902879e294cd777
Фокина Дарья <cool.russia-33@yandex.ru>
2023-06-20 13:33:48 +0300

    added all

---
+++ cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    projectId: "j6dxff",
+    baseUrl: "http://qamid.tmweb.ru",
+    retries: {
+      openMode: 0,
+    },
+    env: {
+      viewportWidth: 1680,
+      viewportHeight: 1050,
+    },
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
